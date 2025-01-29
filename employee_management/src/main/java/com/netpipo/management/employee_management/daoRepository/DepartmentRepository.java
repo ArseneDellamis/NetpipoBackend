a3764bcf4e36cf0d8068f696d52e9044e785a314
Arsene Nyirinkwaya <dellamis11@gmail.com>
2025-01-29 10:22:46 +0200

    adding @Repository to my repositories

--- employee_management/src/main/java/com/netpipo/management/employee_management/daoRepository/DepartmentRepository.java
+++ employee_management/src/main/java/com/netpipo/management/employee_management/daoRepository/DepartmentRepository.java
@@ -2,9 +2,11 @@ package com.netpipo.management.employee_management.daoRepository;
 
 import com.netpipo.management.employee_management.manage.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     List<Department> findByNameContainingIgnoreCase(String name);
--- employee_management/src/main/java/com/netpipo/management/employee_management/daoRepository/EmployeeRepository.java
+++ employee_management/src/main/java/com/netpipo/management/employee_management/daoRepository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.netpipo.management.employee_management.daoRepository;
 
 import com.netpipo.management.employee_management.manage.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     List<Employee>findByDepartment(String department);
