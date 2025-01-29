aaab36542a4f10e5a2bb7d036431d18cd585b18e
Arsene Nyirinkwaya <dellamis11@gmail.com>
2025-01-29 08:27:53 +0200

    adding creating employee and department entity classes and database connection

---
+++ employee_management/src/main/java/com/netpipo/management/employee_management/manage/Employee.java
@@ -0,0 +1,32 @@
+package com.netpipo.management.employee_management.manage;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy =
+    GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+    @Column(nullable = false)
+    private String salary;
+}
