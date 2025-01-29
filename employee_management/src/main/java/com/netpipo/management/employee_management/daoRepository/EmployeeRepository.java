537b81de1cf1e38222e40ec5d3e960f2dc236eac
Arsene Nyirinkwaya <dellamis11@gmail.com>
2025-01-29 10:59:37 +0200

    creating new query in repository for search and updating EmployeeService

--- employee_management/src/main/java/com/netpipo/management/employee_management/daoRepository/EmployeeRepository.java
+++ employee_management/src/main/java/com/netpipo/management/employee_management/daoRepository/EmployeeRepository.java
@@ -1,7 +1,10 @@
 package com.netpipo.management.employee_management.daoRepository;
 
+import com.netpipo.management.employee_management.manage.Department;
 import com.netpipo.management.employee_management.manage.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +12,14 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    List<Employee>findByDepartment(String department);
-    List<Employee> findByNameContainingIgnoreCase(String name);
+    List<Employee> findByDepartment(Department department);
+
+    List<Employee> findByFirstNameContainingIgnoreCase(String firstName);
+    List<Employee> findByLastNameContainingIgnoreCase(String lastName);
+
+
+    @Query("SELECT e FROM Employee e WHERE LOWER(CONCAT(e.firstName, ' ', e.lastName)) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<Employee> searchByFullName(@Param("name") String name);
+
 }
+
--- employee_management/src/main/java/com/netpipo/management/employee_management/service/EmployeeService.java
+++ employee_management/src/main/java/com/netpipo/management/employee_management/service/EmployeeService.java
@@ -1,23 +1,25 @@
 package com.netpipo.management.employee_management.service;
 
+import com.netpipo.management.employee_management.daoRepository.DepartmentRepository;
 import com.netpipo.management.employee_management.daoRepository.EmployeeRepository;
+import com.netpipo.management.employee_management.manage.Department;
 import com.netpipo.management.employee_management.manage.Employee;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepo;
+    private final DepartmentRepository departmentRepo;
 
     @Autowired
-    public EmployeeService(EmployeeRepository employeeRepo) {
+    public EmployeeService(EmployeeRepository employeeRepo, DepartmentRepository departmentRepo) {
         this.employeeRepo = employeeRepo;
+        this.departmentRepo = departmentRepo;
     }
 
     public Employee createEmployee(Employee employee) {
@@ -25,9 +27,7 @@ public class EmployeeService {
     }
 
     public List<Employee> getAllEmployees() {
-
         return employeeRepo.findAll();
-
     }
 
     public Optional<Employee> getEmployeeById(long id) {
@@ -35,35 +35,43 @@ public class EmployeeService {
     }
 
     public List<Employee> searchEmployeesByName(String name) {
-        return employeeRepo.findByNameContainingIgnoreCase(name);
+        List<Employee> employeesByFirstName = employeeRepo.findByFirstNameContainingIgnoreCase(name);
+        List<Employee> employeesByLastName = employeeRepo.findByLastNameContainingIgnoreCase(name);
+        List<Employee> employeesByFullName = employeeRepo.searchByFullName(name);
+
+        // Combine all results into one list without duplicates
+        Set<Employee> resultSet = new HashSet<>();
+        resultSet.addAll(employeesByFirstName);
+        resultSet.addAll(employeesByLastName);
+        resultSet.addAll(employeesByFullName);
+
+        return new ArrayList<>(resultSet);
     }
 
-    public List<Employee> getEmployeesByDepartment(String department) {
+    public List<Employee> getEmployeesByDepartment(String departmentName) {
+        Department department = departmentRepo.findByName(departmentName)
+                .orElseThrow(() -> new RuntimeException("Department Not Found"));
+
         return employeeRepo.findByDepartment(department);
     }
 
-    public Employee updateEmployee( long id,Employee updatedEmployee) {
+    public Employee updateEmployee(long id, Employee updatedEmployee) {
+        Employee employeeToUpdate = employeeRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Not Found"));
 
-        Employee getEmployeeToUpdateById = employeeRepo.findById(id)
-                .orElseThrow(
-                        () -> new RuntimeException("Employee Not Found")
-                );
-        getEmployeeToUpdateById.setFirstName(updatedEmployee.getFirstName());
-        getEmployeeToUpdateById.setLastName(updatedEmployee.getLastName());
-        getEmployeeToUpdateById.setEmail(updatedEmployee.getEmail());
-        getEmployeeToUpdateById.setDepartment(updatedEmployee.getDepartment());
-        getEmployeeToUpdateById.setSalary(updatedEmployee.getSalary());
+        employeeToUpdate.setFirstName(updatedEmployee.getFirstName());
+        employeeToUpdate.setLastName(updatedEmployee.getLastName());
+        employeeToUpdate.setEmail(updatedEmployee.getEmail());
+        employeeToUpdate.setDepartment(updatedEmployee.getDepartment());
+        employeeToUpdate.setSalary(updatedEmployee.getSalary());
 
-
-        return employeeRepo.save(getEmployeeToUpdateById);
-
+        return employeeRepo.save(employeeToUpdate);
     }
 
     public void deleteEmployee(long id) {
-
-        Employee getEmployeeById = employeeRepo.findById(id)
-                        .orElseThrow(()-> new RuntimeException("Employee Id not found"));
-        employeeRepo.deleteById(id);
+        Employee employee = employeeRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Id not found"));
+        employeeRepo.delete(employee);
     }
-
 }
+
